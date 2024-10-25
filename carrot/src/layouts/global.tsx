3abc0731f67f15d530544ba3f416f4078a5e91b7
seyeon326 <lseyeon0723@gmail.com>
2024-10-25 21:40:51 +0900

    fix: padding, BottomNav z-index

--- carrot/src/layouts/global.tsx
+++ carrot/src/layouts/global.tsx
@@ -6,7 +6,7 @@ const GlobalLayout = () => {
   return (
     <div className="bg-black h-full">
       <div className="max-w-[600px] min-h-screen m-auto bg-white h-full overflow-x-hidden scrollbar-hide">
-        <div className="relative p-7 h-full">
+        <div className="relative p-4 h-full">
           <Outlet />
         </div>
         <BottomNav />
