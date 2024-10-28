6aae3bdf324bcf0c967adf1c911c23dcb99d4372
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-28 11:31:13 +0900

    fix: 변경사항

--- carrot/src/router/index.tsx
+++ carrot/src/router/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, RouteObject } from "react-router-dom";
 import Home from "../pages/home";
@@ -11,9 +10,7 @@ import { PaymentSuccessPage } from "../pages/payment/payment/paymentsuccess";
 import { BrandpaySuccessPage } from "../pages/payment/brandpay/brandpaysuccess";
 import { WidgetCheckoutPage } from "../pages/payment/widget/widgetcheckout";
 import { WidgetSuccessPage } from "../pages/payment/widget/widgetsuccess";
-=======
 import { createBrowserRouter, RouteObject } from "react-router-dom";
->>>>>>> 161e4dcc991f38ad9e8f027f67af7c019257ca47
 
 type CustomRouteObject = {
   label?: string;
@@ -23,20 +20,16 @@ type CustomRouteObject = {
 const ROUTES: CustomRouteObject[] = [
   {
     path: "/",
-<<<<<<< HEAD
     element: <GlobalLayout />,
-=======
     element: <HomePage />,
     label: "홈",
     show: true,
->>>>>>> 161e4dcc991f38ad9e8f027f67af7c019257ca47
     children: [
       { path: "/", element: <Home />, label: "홈", show: true },
       { path: "/payment", element: <Container />, label: "결제 페이지", show: true },
     ],
   },
   {
-<<<<<<< HEAD
     path: "widget",
     children: [
       {
@@ -78,7 +71,6 @@ const ROUTES: CustomRouteObject[] = [
   {
     path: "fail",
     element: <FailPage />,
-=======
     path: "/daily",
     element: <DailyLife />,
     label: "동네생활",
@@ -89,7 +81,6 @@ const ROUTES: CustomRouteObject[] = [
     element: <MyPage />,
     label: "마이페이지",
     show: true,
->>>>>>> 161e4dcc991f38ad9e8f027f67af7c019257ca47
   },
 ];
 
